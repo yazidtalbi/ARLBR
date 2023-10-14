f83152f29aa67280ba895ce6d638142a104f4a8d
Yazid <mti.yazid@gmail.com>
2023-10-14 16:32:47 +0100

    fade

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
